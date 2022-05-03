0e00e6293d20cb144acbca30d5c1475d43e00e5b
Cai.0 <caioalvesfernandes@gmail.com>
2022-05-02 21:27:42 -0300

    pong/: Creating a table with the paddle's data, still no interaction

--- pong/main.lua
+++ pong/main.lua
@@ -5,9 +5,22 @@ WINDOW_SIZE = {
     RENDER_HEIGHT = 600,
     RENDER_WIDTH = 800
 }
-local padding = 0
-love.graphics.setDefaultFilter('nearest', 'nearest')
+local WINDOW_MARGIN = 0
+player1 = {
+    x = 0,
+    y = (WINDOW_SIZE.VIRTUAL_WIDTH / 2) - (38 / 2),
+    thickness = 2,
+    height = 38
+}
+player2 = {
+    x = WINDOW_SIZE.VIRTUAL_WIDTH - (2),
+    y = (WINDOW_SIZE.VIRTUAL_HEIGHT / 2) - (38 / 2),
+    thickness = 2,
+    height = 38
+}
+
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
     push:setupScreen(WINDOW_SIZE.VIRTUAL_WIDTH, WINDOW_SIZE.VIRTUAL_HEIGHT, WINDOW_SIZE.RENDER_WIDTH, WINDOW_SIZE.RENDER_HEIGHT, {
         fullscreen = false,
         resizable = true,
@@ -19,8 +32,8 @@ end
 function love.draw()
     love.graphics.setBackgroundColor(40, 40, 40, 255)
     push:apply('start')
-        love.graphics.rectangle('fill', padding, WINDOW_SIZE.VIRTUAL_HEIGHT/2, 12, 38)
-        love.graphics.rectangle('fill', WINDOW_SIZE.VIRTUAL_WIDTH - padding - 12, WINDOW_SIZE.VIRTUAL_HEIGHT/2, 12, 38)
+        love.graphics.rectangle('fill', player1.x, player1.y, player1.thickness, player1.height)
+        love.graphics.rectangle('fill', player2.x, player2.y, player2.thickness, player2.height)
     push:apply('end')
 end
 
